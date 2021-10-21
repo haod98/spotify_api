21f3d02cac94ac2fed8b29f7bc8ce3c47fc120a0
Hao <74541600+haod98@users.noreply.github.com>
2021-10-21 15:44:03 +0200

    Show top tracks

--- src/index.js
+++ src/index.js
@@ -11,12 +11,29 @@ const headers = new Headers();
 headers.set('Authorization', `Bearer ${access_token}`);
 
 
-console.log(access_token);
+const contentBody = document.querySelector('.content');
+
+console.log(contentBody);
 spotifyApi.setAccessToken(access_token);
-spotifyApi.getMyTopTracks({limit: 10}, (err, data) => {
+spotifyApi.getMyTopTracks({ limit: 10 }, (err, data) => {
   if (err) {
     console.log(err);
   } else {
     console.log(`Artist albums`, data);
+    for (let i = 0; i < data.items.length; i++) {
+      const artistName = data.items[i].artists[0].name;
+      const favouriteTrack = data.items[i].name;
+      const albumCoverSrc = data.items[i].album.images[1].url;
+      const urlTrack = data.items[i].external_urls.spotify;
+      contentBody.innerHTML += `
+      <div>
+        <p><span> ${i + 1} </span> ${artistName} - ${favouriteTrack} </p>
+        <img src="${albumCoverSrc}" alt="Album Cover from ${artistName} Song: ${favouriteTrack}">
+        <a href="${urlTrack}" target="_blank">Listen ${artistName} - ${favouriteTrack}</a>
+      </div>
+      `
+      console.log(urlTrack);
+
+    }
   }
-})
+});
